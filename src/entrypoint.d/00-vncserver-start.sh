26acf1ba0429d059fb4cc053474fd4e028e74ad5
Matthew Davis <matthew@appsoa.io>
2017-01-29 02:08:29 +0000

    yo

--- src/entrypoint.d/00-vncserver-start.sh
+++ src/entrypoint.d/00-vncserver-start.sh
@@ -3,4 +3,4 @@ DISPLAY=:1
 /bin/vncserver-ctl.sh
 # ./utils/launch.sh --listen $CONF_VNC_HTTP_PORT --vnc $CONF_VNC_REMOTE
 cd /home/user/noVNC
-./utils/launch.sh --listen 6901 --vnc 127.0.0.1:5901 &
+./utils/launch.sh --listen 6902 --vnc 127.0.0.1:5901 &
